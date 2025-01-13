168d69296a341c71c0707f30c0c05f7ba1ae29f4
Abhay yadav <yadavabhay8227.com>
2025-01-13 18:58:25 +0530

    3

--- run.py
+++ run.py
@@ -1,17 +1,18 @@
+import os
 from flask import Flask
 
 def create_app():
     app = Flask(__name__)
 
-    # Simple route to confirm backend is running
     @app.route('/')
     def home():
         return "Hello Abhay.. Your backend is running"
 
     return app
 
-app = create_app()  # Gunicorn looks for this "app"
+app = create_app()
 
-# Uncomment the following line if you want to run it locally
-# if __name__ == '__main__':
-#     app.run(debug=True)
+if __name__ == "__main__":
+    # Use the port from the environment or default to 5000
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
