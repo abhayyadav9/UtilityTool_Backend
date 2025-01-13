bec9f39f6aa22d0e504c8d99fd24cd702e25eed0
Abhay yadav <yadavabhay8227.com>
2025-01-13 18:32:18 +0530

    1

--- run.py
+++ run.py
@@ -1,6 +1,17 @@
-from app import create_app
+from flask import Flask
 
-app = create_app()
+def create_app():
+    app = Flask(__name__)
 
+    # Simple route to confirm backend is running
+    @app.route('/')
+    def home():
+        return "Backend is running successfully!"
+
+    return app
+
+app = create_app()  # Gunicorn looks for this "app"
+
+# Uncomment the following line if you want to run it locally
 # if __name__ == '__main__':
 #     app.run(debug=True)
