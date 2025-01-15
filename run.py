89eac8c747b96f85148a4dc11a1e3bfe65a53773
Abhay yadav <yadavabhay8227.com>
2025-01-15 22:41:27 +0530

    2

--- run.py
+++ run.py
@@ -1,8 +1,7 @@
 import os
 from flask import Flask
-from app import create_app
 
-def create_app1():
+def create_app():
     app = Flask(__name__)
 
     @app.route('/')
@@ -15,6 +14,5 @@ app = create_app()
 
 if __name__ == "__main__":
     # Use the port from the environment or default to 5000
-    port = int(os.environ.get("PORT", 5000))  # Ensure the port is dynamically set
-    app.run(host='0.0.0.0', port=port)
-  
+    # port = int(os.environ.get("PORT", 5000))  # Ensure the port is dynamically set
+    app.run(host='0.0.0.0', port=5000)
