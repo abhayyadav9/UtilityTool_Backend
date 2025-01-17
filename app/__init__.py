a73b118ced90311cea764c0b5807e9ac0a4d0fa8
Abhay yadav <yadavabhay8227.com>
2025-01-17 21:46:26 +0530

    fix 3

--- app/__init__.py
+++ app/__init__.py
@@ -12,7 +12,7 @@ def create_app():
     from app.routes.remove_background import remove_bg_bp
     from app.routes.qr_code import qr_code_bp
     from app.routes.greet import greet_bp
-    from app.routes.doc_pdf import converter
+    # from app.routes.doc_pdf import converter
     from app.routes.pdf_doc import pdf_converter
     from app.routes.enhance import image_enhancer
     from app.routes.resizer import image_resizer
@@ -23,7 +23,7 @@ def create_app():
     app.register_blueprint(greet_bp)
     app.register_blueprint(qr_code_bp)
     app.register_blueprint(remove_bg_bp)
-    app.register_blueprint(converter, url_prefix='/converter')  # Registered with /converter
+    # app.register_blueprint(converter, url_prefix='/converter')  # Registered with /converter
     app.register_blueprint(pdf_converter, url_prefix='/pdfconverter')
     app.register_blueprint(image_enhancer)
     app.register_blueprint(image_resizer)
