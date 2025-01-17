c019b9aad1ca1de8e13cfd8e3dd54497d3c1be3d
Abhay yadav <yadavabhay8227.com>
2025-01-17 22:54:53 +0530

    ac

--- run.py
+++ run.py
@@ -31,5 +31,5 @@ app = create_app()
 
 if __name__ == "__main__":
     # Use the port from the environment or default to 5000
-    port = int(os.environ.get("PORT", 5000))
-    app.run(host='0.0.0.0', port=port)
+    # port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=5000)
